1126d7705c69b4a6cb7ae1d69418085230edb657
Madison Williams <wmadi20@yahoo.com>
2018-03-11 10:32:56 -0500

    initial commit

---
+++ Basics.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace CLanguageBasics.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+            const byte sample1 = 0x3A;
+            byte sample2 = 58;
+            int heartRate = 85;
+            double deposits = 135002796.0;
+            const float acceleration = 9.800f;
+            float mass = 14.6f;
+            double distance = 129.763001;
+            bool lost = true;
+            bool expensive = true;
+            int choice = 2;
+            const char integral = '\u222B';
+            const string greeting = "Hello";
+            string name = "Karen";
+    
+            if(sample1==sample2)
+            {
+                Console.WriteLine("The samples are equal");
+            }
+            else{
+                Console.WriteLine("The samples are not equal");
+            }
+    
+    if(heartRate>=40 && heartRate<=80)
+    {
+                Console.WriteLine("Heart rate is normal.");
+    }
+    else{
+                Console.WriteLine("Heart rate is not normal.");
+    }
+    
+    if(deposits>=100000000 )
+    {
+                Console.WriteLine("You are exceedingly wealthy.");
+    }
+    else{
+                Console.WriteLine("Sorry you are so poor.");
+    }
+    
+            float force = mass * acceleration;
+            Console.WriteLine("force = {0}",force);
+            Console.WriteLine("{0} is the distance",distance);
+    
+    if(lost && expensive)
+    {
+                Console.WriteLine("I am really sorry! I will get the manager.");
+    }
+    else if (lost && !expensive){
+                Console.WriteLine("Here is coupon for 10% off.");
+    }
+    switch (choice) {
+                case 1: Console.WriteLine("You chose 1.");
+                    break;
+                case 2: Console.WriteLine("You chose 2.");
+                    break;
+                case 3: Console.WriteLine("You chose 3.");
+                    break;
+                default: Console.WriteLine("You made an unknown choice.");
+                    break;
+    }
+            Console.WriteLine("{0} is an integral",integral);
+                   for (int i = 1; i <= 5;i++)
+                    {
+                Console.WriteLine(i);
+                    }
+
+    int age =0;
+    while(age<6){
+                Console.WriteLine("age = {0}",age);
+                age++;
+    }
+            Console.WriteLine("{0} {1}", greeting, name);
+        }
+    }
+}
